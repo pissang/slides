ea4ad7a5670d0b6de23eec242e5ba59ef8a73c68
pissang <bm2736892@gmail.com>
2017-07-18 11:52:01 +0800

    Add echarts-gl

---
+++ topics/echarts-gl/asset/data/buildings-simplify.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var buildingsData = JSON.parse(fs.readFileSync('./buildings.json', 'utf-8'));
+var compress = require('./compress');
+
+var features = buildingsData.features.map(function (feature, idx) {
+    return {
+        type: 'Feature',
+        properties: {
+            name: idx.toString(),
+            height: +feature.properties.EW_HA2013 / 10
+        },
+        geometry: feature.geometry
+    };
+}).slice(0, 10000);
+
+fs.writeFileSync('buildings.json.js', 'buildingsGeoJSON = ' + JSON.stringify({ features: features }), 'utf-8');
